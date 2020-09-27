6e5b6374d6359e6c9b0899ed11c24c6589fb3097
Kaj <69725937+KaiTech-git@users.noreply.github.com>
2020-09-27 15:03:58 +0200

    Add files via upload

--- plot4.R
+++ plot4.R
@@ -8,10 +8,12 @@ Data$DateTime <- strptime(Data$DateTime, "%Y-%m-%d %H:%M:%S")
 par(mfrow = c(2,2))
 with(Data,plot(DateTime,Global_active_power, type = "l", 
                xlab = "", ylab = "Global Active Power (kilowatts)")) # first plot
-with(Data, plot(DateTime, Voltage, type = "l")) # second plot
+with(Data, plot(DateTime, Voltage, type = "l", xlab = "datetime")) # second plot
 with(Data, plot(DateTime, Sub_metering_1, type = "l", ylab = "energy sub metering", xlab = "")) # third plot
 lines(Data$DateTime, Data$Sub_metering_2, col ="red")
 with(Data, lines(DateTime, Sub_metering_3, col = "blue"))
 legend(col = c("black", "red", "blue"), lty = 1,  
-       legend =c( "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")  , "topright", cex = 0.5)
-with(Data, plot(DateTime, Global_reactive_power, type = "l")) #4th plot
+       legend =c( "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")  , "topright", cex = 0.5, bty ="n")
+with(Data, plot(DateTime, Global_reactive_power, type = "l", xlab = "datetime")) #4th plot
+dev.copy(png, file="plot4.PNG")
+dev.off()
